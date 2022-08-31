8aec998bd3ace3326e2515ed71facdb37932808f
Olga Kassian <olga.kassian@valtech.com>
2022-08-31 14:58:12 +0100

    Fixed Header Height

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,6 +11,8 @@ export class FormComponent {
 
   registerForm = new FormGroup({
     companyName: new FormControl(''),
+    companyNIF: new FormControl(''),
+    legalAddress: new FormControl(''),
     teamName: new FormControl(''),
     playerName1: new FormControl(''),
     playerEmail1: new FormControl(''),
@@ -18,8 +20,6 @@ export class FormComponent {
     playerEmail2: new FormControl(''),
     playerName3: new FormControl(''),
     playerEmail3: new FormControl(''),
-    playerName4: new FormControl(''),
-    playerEmail4: new FormControl(''),
     responsibleEmail: new FormControl(''),
   });
 
