4bb1ecbddd35d70f8d8dd93b663585ed5cec0f9a
Olga Kassian <olga.kassian@valtech.com>
2022-09-05 09:09:15 +0100

    Updated Dummy Data

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 declare let Email: any;
-
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -24,23 +23,38 @@ export class FormComponent {
     responsibleEmail: new FormControl(''),
   });
 
+  isEmailSent = false;
+  isFormValid = true;
+
   constructor() {}
 
   submitForm() {
-    console.log(this.registerForm);
     const formValues = this.registerForm.value;
 
-    Email.send({
-      SecureToken: '5612e7ec-0912-46f7-81f4-c2b7814a998c',
-      To: 'olgakassian@gmail.com',
-      From: 'olgakassian@gmail.com',
-      Subject: `New Submission from ${formValues.companyName}`,
-      Body: `
+    if (this.registerForm.valid) {
+      Email.send({
+        SecureToken: '8ffd422a-d437-45ed-be76-906ae607a4af',
+        To: 'carolina.cerejo@valtech.com',
+        From: 'carolina.cerejo@valtech.com',
+        Subject: `New Submission from ${formValues.companyName}`,
+        Body: `
       Company Name: ${formValues.companyName}, <br>
       NIF: ${formValues.companyNIF},
       Legal Address: ${formValues.legalAddress},
       Team Name: ${formValues.teamName}
       `,
-    }).then((message: any) => alert(message));
+      }).then((message: any) => {
+        if (message === 'OK') {
+          this.isEmailSent = true;
+        }
+      });
+      this.isFormValid = true;
+    } else {
+      this.isFormValid = false;
+    }
+
+    this.registerForm.updateValueAndValidity();
+
+    console.log(this.registerForm);
   }
 }
